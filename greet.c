ba71a96f503a3bfd9dbff676c409f72b5f1314bf
Bill Morgan <arthurwilliammorgan@gmail.com>
2024-12-14 14:11:27 -0600

    Remove mathlib library and associated files from the project

--- greet.c
+++
@@ -1,4 +0,0 @@
-// greet.c
-#include <stdio.h>
-
-void greet() { printf("Greetings from greet.c!\n"); }
--- mathlib/mathutils.c
+++
@@ -1,5 +0,0 @@
-#include "mathutils.h"
-
-int add(int a, int b) { return a + b; }
-
-int subtract(int a, int b) { return a - b; }
